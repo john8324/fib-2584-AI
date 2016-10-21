2be9d8f019092b3f8fee5aa3c2e0ddedaaf7f188
john8324 <ay12345678@gmail.com>
2016-10-21 08:57:56 +0800

    bug fixed!!!! bug is max_dir

--- Fib2584Ai.cpp
+++ Fib2584Ai.cpp
@@ -16,7 +16,7 @@ void Fib2584Ai::initialize(int argc, char* argv[])
 MoveDirection Fib2584Ai::generateMove(const int board[4][4])
 {
 	MyBoard after[] = {board, board, board, board};
-	int max_dir = -1, max_r = 0;
+	int max_dir = -1, max_r = 0; // (dir and r) of max_score
 	double max_score = -1e300;
 	for (int i = 0; i < 4; i++) {
 		int r = 0;
@@ -25,11 +25,18 @@ MoveDirection Fib2584Ai::generateMove(const int board[4][4])
 			if (score > max_score) {
 				max_score = score;
 				max_r = r;
+				max_dir = i;
 			}
 		}
 	}
+	if (max_dir == -1) {
+		cout << "board CANNOT move!!!!!!!!!!" << endl;
+		cout << MyBoard(board);
+		cout << "---------------------------" << endl;
+		throw ("FAIL to gen move");
+	}
 	// max_r = max_dir == -1 ? 0 : max_r;
-	max_dir = max_dir == -1 ? rand() % 4 : max_dir;
+	//max_dir = max_dir == -1 ? rand() % 4 : max_dir;
 	//cout << "r = 1\nafter = \n" << after[max_dir] << endl;
 	//cout << "max_r = " << max_r << endl;
 	td.pushAfterState(State(after[max_dir], max_r));
