11fec03f043f458ca7f8eba391d2fc38cbf1b910
john8324 <ay12345678@gmail.com>
2017-01-15 18:08:55 +0800

    generateMove 改回 project2

--- Fib2584Ai.cpp
+++ Fib2584Ai.cpp
@@ -67,22 +67,33 @@ void Fib2584Ai::initialize(int argc, char* argv[])
 	srand(time(NULL));
 }
 
-MoveDirection Fib2584Ai::generateMove( int board[4][4] )
+MoveDirection Fib2584Ai::generateMove(int board[4][4])
 {
 	MyBoard after[] = {board, board, board, board};
-	int max_dir = -1;
+	int max_dir = -1, max_r = 0; // (dir and r) of max_score
 	double max_score = -1e300;
 	for (int i = 0; i < 4; i++) {
 		int r;
 		if (after[i].move(static_cast<MoveDirection>(i), r)) {
-			const double score = after[i].zeroCount() + 0.001 * after[i].maxTile();
+			const double score = r + td.eval(Feature(after[i]));
 			if (score > max_score) {
 				max_score = score;
+				max_r = r;
 				max_dir = i;
 			}
 		}
 	}
-	move_count = max_dir == -1 ? 0 : move_count;
+	if (max_dir == -1) {
+		cout << "board CANNOT move!!!!!!!!!!" << endl;
+		cout << MyBoard(board);
+		cout << "---------------------------" << endl;
+		throw ("FAIL to gen move");
+	}
+	// max_r = max_dir == -1 ? 0 : max_r;
+	//max_dir = max_dir == -1 ? rand() % 4 : max_dir;
+	//cout << "r = 1\nafter = \n" << after[max_dir] << endl;
+	//cout << "max_r = " << max_r << endl;
+	td.pushAfterState(State(after[max_dir], max_r));
 	return static_cast<MoveDirection>(max_dir);
 }
 
